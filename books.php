f46b626f638c534e86a2e85a5ac38a3735a609a4
Iterl0 <Iterl054@gmail.com>
2017-02-05 00:58:52 +0200

    update_04

--- books.php
+++ books.php
@@ -2,12 +2,6 @@
 include('core.php');
 login_stat();
 
-print_r($_SESSION['user']);
-
-echo "<br>";
-echo "<br> book id>" . $book_id;
-
-
 if (isset($_GET['book'])) {
     $book_id = $_GET['book'];
     $book_edit = $_SESSION['books_arr'][$book_id];
@@ -22,7 +16,6 @@ if (isset($_GET['book'])) {
     $button_label = 'Create';
 }
 
-
 if (!empty($_SESSION['current_book'])) {
     if ($_SESSION['current_book']['action'] == 'Update') {
         unset($_SESSION['current_book']['action']);
@@ -36,60 +29,38 @@ if (!empty($_SESSION['current_book'])) {
         unset($_SESSION['current_book']);
 
     }
-
-
-
 }
 
 
 
-
-echo "<br>";
-echo "<br> session>" . print_r($_SESSION);
-
-echo "<br>";
-echo "<br> post>" . print_r($_POST);
-
-echo "<br>";
-echo "<br> get>" . print_r($_GET);
-
-echo "<br>" . sizeof($_SESSION['books_arr']);
 $count = sizeof($_SESSION['books_arr']);
 
-//function add_row($count){
-//
-//    for ($i=0; $i<$count; $i++) {
-//        echo "<tr>";
-//            for ($k=0; $k<=3; $k++) {
-//                echo "
-//                <td class='text-left'>" . $_SESSION['books_arr'][$i][$k] . "</td>
-//                ";
-//            }
-//        echo "</tr>";
-//    }
-//    //    return $count!==1 ? add_row($count-1) : null;
-//}
-
 function add_row () {
-
     foreach ($_SESSION['books_arr'] as $sup_arr => $key){
-            $edit = '<a href="/php-bookshelf.com/books.php?book=' . $sup_arr . '">Edit</a>';
+
+
+
+            $edit = '<a href="books.php?book=' . $sup_arr . '" data-toggle="modal" data-target="#myModal">Edit</a>';
+//            $edit = '
+//                     <form action="books.php" method="get">
+//                     <button type="submit" name="book" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+//                     <input type="text" name="book" value=" '. $sup_arr .'">
+//                     </form>
+//                    ';
+//            $edit = '
+//
+//                         <button type="submit" name="toggle_edit" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+//                         <input type="text" name="toggle_edit" value=" '. $sup_arr .'">
+//
+//                        ';
             echo "<tr>"
                     . "<td class='text-left'>" . $key['in_name'] . "</td>"
                     . "<td class='text-left'>" . $key['in_auth'] . "</td>"
                     . "<td class='text-left'>" . $key['in_date'] . "</td>"
-                    . "<td class='text-left'>  $edit  </td>";
-            echo "<tr>";
-//        }
+                    . "<td class='text-left'>" . $edit .  "</td>";
+            echo "</tr>";
     }
 }
-
-//$in_name = $_POST['in_name'] ? $_POST['in_name'] : null;
-//$in_auth = $_POST['in_auth'] ? $_POST['in_auth'] : null;
-//$in_date = $_POST['in_date'] ? $_POST['in_date'] : null;
-
-
-//$_SESSION['current_book'][$key] = $book;
 
 
 ?>
@@ -100,8 +71,9 @@ function add_row () {
         <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <script type="text/javascript" src="js/js.js">
-        </script>
+        <script type="text/javascript" src="js/js.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <meta charset="utf-8" />
         <title>Table Style</title>
         <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, width=device-width">
@@ -109,46 +81,115 @@ function add_row () {
 
     <body>
 
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+<!--                <a class="navbar-brand" href="#">Brand</a>-->
+            </div>
 
-                <div class="table-title">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="books.php">Books<span class="sr-only">(current)</span></a></li>
+                    <li><a href="image.php">Image</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">One more separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                </ul>
+                <form class="navbar-form navbar-right">
+                    <button type="submit" class="btn btn-default" formaction="logout.php">Logout</button>
+                </form>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+
+
+
+
+
+    <!-- Trigger the modal with a button -->
+
+
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog" id="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content" id="popup">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Edit <?php echo $b_name . "&nbsp" . $b_auth . "&nbsp" . $b_date ?></h4>
+                </div>
+                <div class="modal-body">
+                    <form id="book-reg" action="books.php" method="post">
+                        <div class="container">
+                            <div class="col-md-3">
+                                <label for="in_name">Book name</label>
+                                <input type="text" class="form-control" required="" name="in_name" value="<?php echo $b_name ?>">
+                            </div>
+                            <div class="col-md-3">
+                                <label for="in_auth">Author</label>
+                                <input type="text" class="form-control" required="" name="in_auth" value="<?php echo $b_auth ?>">
+                            </div>
+                            <div class="col-md-3">
+                                <label for="in_date">Date of publication</label>
+                                <input type="text" class="form-control" required="" name="in_date" value="<?php echo $b_date ?>">
+                            </div>
+                            <?php if(isset($book_id)){ ?>
+                                <input type="text" name="book_id" value="<?php echo $book_id ?>" />
+                            <?php } ?>
+                            <div class="col-md-3">
+                                <button type="submit" class="btn btn-default btn-icon" name="action" formaction="update_table.php" value="<?php echo $button_label ?>"><?php echo $button_label ?></button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+<!--                    <button type="submit" class="btn btn-default btn-icon" data-dismiss="modal" name="action" formaction="update_table.php" value="--><?php //echo $button_label ?><!--">--><?php //echo $button_label ?><!--</button>-->
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+        <div class="table-title">
             <h3>Data Table</h3>
         </div>
-                <div class="container">
-                    <div class="row">
-                        <div>
-                            <div class="container" id="edit_form">
-                                <div class="row">
-                                    <div class="modal-content">
-                                            <div class="modal-body">
-                                                <div class="row">
-                                                    <form id="book-reg" action="books.php" method="post">
-                                                        <div class="col-md-3">
-                                                                <label for="in_name">Book name</label>
-                                                                <input type="text" class="form-control" required="" name="in_name" value="<?php echo $b_name ?>">
-                                                        </div>
-                                                        <div class="col-md-3">
-                                                                <label for="in_auth">Author</label>
-                                                                <input type="text" class="form-control" required="" name="in_auth" value="<?php echo $b_auth ?>">
-                                                        </div>
-                                                        <div class="col-md-3">
-                                                                <label for="in_date">Date of publication</label>
-                                                                <input type="text" class="form-control" required="" name="in_date" value="<?php echo $b_date ?>">
-                                                        </div>
-                                                        <?php if(isset($book_id)){ ?>
-                                                            <input type="text" name="book_id" value="<?php echo $book_id ?>" />
-                                                        <?php } ?>
-                                                        <div class="col-md-3">
-                                                                <button type="submit" class="btn btn-default btn-icon" data-dismiss="modal" name="action" formaction="update_table.php" value="<?php echo $button_label ?>"><?php echo $button_label ?></button>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
+
+        <div class="container">
+
+
+
+            <div class="row">
                         <div>
                             <table class="table-fill" id="myTable">
                                 <thead>
@@ -156,31 +197,20 @@ function add_row () {
                                     <th class="text-left">Book</th>
                                     <th class="text-left">Author</th>
                                     <th class="text-left">Date of publication</th>
-                                    <th class="text-left"> </th>
+                                    <th class="text-left"><button type="submit" class="btn btn-info" data-toggle="modal" data-target="#myModal">Create new</button></th>
                                 </tr>
                                 </thead>
-                                <a href="logout.php" class="btn btn-default">log out</a>
-<!--                                --><?php //if (is_admin()): ?><!--  -->
+
+<!--
                                     <tbody id="main_table">
 <!--                                    --><?php //$count!==0 ? add_row($count) : null ?>
                                     <?php $count!==0 ? add_row() : null ?>
-                                    <tr>
-                                        <td class="text-left">January</td>
-                                        <td class="text-left">$ 50,000.00</td>
-                                        <td class="text-left">$ 50,000.00</td>
-                                        <td class="text-left"></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-left">January</td>
-                                        <td class="text-left">$ 50,000.00</td>
-                                        <td class="text-left">$ 50,000.00</td>
-                                        <td class="text-left"></td>
-                                    </tr>
+
                                     </tbody>
-<!--                                --><?php //endif; ?>
+
                             </table>
                         </div>
                     </div>
-                </div>
+        </div>
     </body>
 
---
+++ image.php
@@ -0,0 +1,202 @@
+<?php
+include('core.php');
+login_stat();
+
+//if (isset($_GET['book'])) {
+//    $book_id = $_GET['book'];
+//    $book_edit = $_SESSION['books_arr'][$book_id];
+//    $b_name = $book_edit[in_name];
+//    $b_auth = $book_edit[in_auth];
+//    $b_date = $book_edit[in_date];
+//    $button_label = 'Update';
+//} else {
+//    $b_name = "";
+//    $b_auth = "";
+//    $b_date = "";
+//    $button_label = 'Create';
+//}
+//
+//if (!empty($_SESSION['current_book'])) {
+//    if ($_SESSION['current_book']['action'] == 'Update') {
+//        unset($_SESSION['current_book']['action']);
+//        $book_id = $_SESSION['current_book']['book_id'];
+//        unset($_SESSION['current_book']['book_id']);
+//        $_SESSION['books_arr'][$book_id] = $_SESSION['current_book'];
+//        unset($_SESSION['current_book']);
+//    } else {
+//        unset($_SESSION['current_book']['action']);
+//        $_SESSION['books_arr'][] = $_SESSION['current_book'];
+//        unset($_SESSION['current_book']);
+//
+//    }
+//}
+
+//$count = sizeof($_SESSION['books_arr']);
+//
+//function add_row () {
+//    foreach ($_SESSION['books_arr'] as $sup_arr => $key){
+//        $edit = '<a href="/php-bookshelf.com/books.php?book=' . $sup_arr . '">Edit</a>';
+//        echo "<tr>"
+//            . "<td class='text-left'>" . $key['in_name'] . "</td>"
+//            . "<td class='text-left'>" . $key['in_auth'] . "</td>"
+//            . "<td class='text-left'>" . $key['in_date'] . "</td>"
+//            . "<td class='text-left'>  $edit  </td>";
+//        echo "<tr>";
+//    }
+//}
+//
+
+?>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/js.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <meta charset="utf-8" />
+    <title>Table Style</title>
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, width=device-width">
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!--                <a class="navbar-brand" href="#">Brand</a>-->
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="image.php">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+            </ul>
+            <form class="navbar-form navbar-right">
+                <button type="submit" class="btn btn-default" formaction="logout.php">Logout</button>
+            </form>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+
+<!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Modal Header</h4>
+            </div>
+            <div class="modal-body">
+                <p>Some text in the modal.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+<div class="table-title">
+    <h3>Data Table</h3>
+</div>
+
+<div class="container">
+
+    <!--           start_ edit form-->
+    <?php if (is_admin()): ?>
+        <div class="row">
+            <div class="container" id="edit_form">
+                <div class="row">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <div class="row">
+                                <form id="book-reg" action="books.php" method="post">
+                                    <div class="col-md-3">
+                                        <label for="in_name">Book name</label>
+                                        <input type="text" class="form-control" required="" name="in_name" value="<?php echo $b_name ?>">
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label for="in_auth">Author</label>
+                                        <input type="text" class="form-control" required="" name="in_auth" value="<?php echo $b_auth ?>">
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label for="in_date">Date of publication</label>
+                                        <input type="text" class="form-control" required="" name="in_date" value="<?php echo $b_date ?>">
+                                    </div>
+                                    <?php if(isset($book_id)){ ?>
+                                        <input type="text" name="book_id" value="<?php echo $book_id ?>" />
+                                    <?php } ?>
+                                    <div class="col-md-3">
+                                        <button type="submit" class="btn btn-default btn-icon" data-dismiss="modal" name="action" formaction="update_table.php" value="<?php echo $button_label ?>"><?php echo $button_label ?></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+    <!--           end_ edit form-->
+
+    <div class="row">
+        <div>
+            <table class="table-fill" id="myTable">
+                <thead>
+                <tr>
+                    <th class="text-left">Book</th>
+                    <th class="text-left">Author</th>
+                    <th class="text-left">Date of publication</th>
+                    <th class="text-left"> </th>
+                </tr>
+                </thead>
+
+                <!--
+                                                    <tbody id="main_table">
+                <!--                                    --><?php //$count!==0 ? add_row($count) : null ?>
+                <?php $count!==0 ? add_row() : null ?>
+
+                </tbody>
+
+            </table>
+        </div>
+    </div>
+</div>
+</body>
+
